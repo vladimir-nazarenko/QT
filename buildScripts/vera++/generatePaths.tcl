e379854026a1f01070966ad9403e303e8a050c3a
Vladimir Nazarenko <nazarenko2010@gmail.com>
2013-07-16 13:36:38 +0400

    Illegal option -p deleted

--- buildScripts/vera++/generatePaths.tcl
+++ buildScripts/vera++/generatePaths.tcl
@@ -4,8 +4,7 @@
 proc getFiles { folder } {
   set listOfFiles ""
   foreach sourceFile [glob -nocomplain -type f -directory $folder *] {
-  puts $sourceFile
-    if [regexp -nocase {.*[.]cpp$} $sourceFile ] {
+    if [regexp -nocase {.*[.]cpp$|.*[.]h$} $sourceFile ] {
       set listOfFiles [concat $listOfFiles $sourceFile]
     }
   }
@@ -15,7 +14,6 @@ proc getFiles { folder } {
   return $listOfFiles
 }
 
-set parameters "-p allRules"
 set parameters [concat $parameters [getFiles [pwd]]]
 set fo [open "buildScripts/vera++/params" "w" ]
 puts $fo $parameters
